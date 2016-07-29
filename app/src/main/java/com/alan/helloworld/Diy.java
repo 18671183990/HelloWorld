3cd5a78939d6f705929b4a5b123247e72323bcb5
Alan1 <lixianyong2005@126.com>
2016-07-29 13:16:01 +0800

    the second time to change the content

--- app/src/main/java/com/alan/helloworld/Diy.java
+++ app/src/main/java/com/alan/helloworld/Diy.java
@@ -1,7 +1,12 @@
 package com.alan.helloworld;
 
 /**
- * projectName: 	    HelloWorld packageName:	    com.alan.helloworld className:	        Diy author:	                Alan time: 2016/7/29	13:09 desc: TODO
+ * projectName: 	    HelloWorld
+ * packageName:	    com.alan.helloworld
+ * className:	        Diy
+ * author:	                Alan
+ * time: 2016/7/29	13:09
+ * desc: 测试类
  */
 public class Diy {
 
