a0b2ac1f99a2d862598dc5bafef04fb8c4465ac5
Alan1 <lixianyong2005@126.com>
2016-07-29 13:09:58 +0800

    add an Diy class

---
+++ app/src/main/java/com/alan/helloworld/Diy.java
@@ -0,0 +1,9 @@
+package com.alan.helloworld;
+
+/**
+ * projectName: 	    HelloWorld packageName:	    com.alan.helloworld className:	        Diy author:	                Alan time:
+ * 2016/7/29	13:09 desc:	                    TODO
+ */
+public class Diy {
+
+}
