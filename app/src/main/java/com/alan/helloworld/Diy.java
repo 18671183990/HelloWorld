9b9e16495c9e40f361346d68126ae8ed166d0eec
Alan1 <lixianyong2005@126.com>
2016-07-29 13:12:18 +0800

    change the content

--- app/src/main/java/com/alan/helloworld/Diy.java
+++ app/src/main/java/com/alan/helloworld/Diy.java
@@ -1,8 +1,7 @@
 package com.alan.helloworld;
 
 /**
- * projectName: 	    HelloWorld packageName:	    com.alan.helloworld className:	        Diy author:	                Alan time:
- * 2016/7/29	13:09 desc:	                    TODO
+ * projectName: 	    HelloWorld packageName:	    com.alan.helloworld className:	        Diy author:	                Alan time: 2016/7/29	13:09 desc: TODO
  */
 public class Diy {
 
